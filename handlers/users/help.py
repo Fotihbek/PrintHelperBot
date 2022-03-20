9ac3a1492a95278a591f5cc3c377aa558bf6d604
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-03-20 09:33:24 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,6 +6,6 @@ from loader import dp
 
 @dp.message_handler(commands=["yordam"])
 async def bot_help(message: types.Message):
-    text = ("Hujjatni, birinchi sahifasidan boshlab chop etmoqchi bo'lsangiz, shunchaki, necha bet ekanini yoki nechinchi betgacha chiqarmoqchi ekaningizni kiriting. Agar, hujjatning birinchi betidan emas, orasidan boshlashni ixtiyor qilsangiz, <b>55 100</b> shaklida yuboring! Bunda, ``55-betdan, 100-betgacha chop etish buyrug'i berilgan`` deb qaraladi.")
+    text = ("Hujjatni, birinchi sahifasidan boshlab chop etmoqchi bo'lsangiz, shunchaki, necha bet ekanini yoki nechinchi betgacha chiqarmoqchi ekaningizni kiriting. Agar, hujjatning birinchi betidan emas, orasidan boshlashni ixtiyor qilsangiz, <b>55 100</b> shaklida yuboring! Bunda, ``55-betdan, 100-betgacha chop etish buyrug'i berilgan`` deb qaraladi.\n👨‍💻 Dasturchi: Komilov Fotihbek")
     
     await message.answer(text)
