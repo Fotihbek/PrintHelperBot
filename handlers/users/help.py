320e307bd69c2ed910f30cd29fcff8c4de5bded5
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-03-20 09:25:59 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 from loader import dp
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(commands=["yordam"])
 async def bot_help(message: types.Message):
     text = ("Hujjatni, birinchi sahifasidan boshlab chop etmoqchi bo'lsangiz, shunchaki, necha bet ekanini yoki nechinchi betgacha chiqarmoqchi ekaningizni kiriting. Agar, hujjatning birinchi betidan emas, orasidan boshlashni ixtiyor qilsangiz, <b>55 100</b> shaklida yuboring! Bunda, ``55-betdan, 100-betgacha chop etish buyrug'i berilgan`` deb qaraladi.")
     
