ab912718e871ad40095beba34618775a83fd6de8
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-03-20 09:24:50 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,4 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Salom, {message.from_user.full_name}, hujjat faylining sahifalari sonini kiriting!\nTo'liq ma'lumot /yordam buyrug'ini yuboring.")
