3efa4c2042ababd63c19c319ff59037be5105ace
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-09-13 17:05:45 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,12 +7,12 @@ from loader import dp, db
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Xush kelibsiz, {message.from_user.full_name}! Marhamat, hujjatning sahifalari sonini kiritishingiz mumkin. Samarali foydalanish haqida: /yordam buyrug'ini yuboring")
-    try:
+    await message.answer(f"Xush kelibsiz, {message.from_user.full_name}! Iltimos telefon raqamingizni kiriting")
+"""    try:
         await db.add_user(telegram_id=message.from_user.id,
                                  full_name=message.from_user.full_name,
                                  username=message.from_user.username)
     except asyncpg.exceptions.UniqueViolationError:
         await db.update_user_fullname(username=message.from_user.full_name,
                                  telegram_id=message.from_user.id
-                                 )
+                                 )"""
